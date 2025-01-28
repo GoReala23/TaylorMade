67bd39e56dd55dfee00f29e6377ced99bb5a1692
Hassan Porter <90788434+GoReala23@users.noreply.github.com>
2025-01-27 19:42:21 -0500

    Updates

--- backend/utils/config.js
+++
@@ -1,6 +0,0 @@
-const { NODE_ENV, JWT_SECRET = 'super-strong-secret' } = process.env;
-
-module.exports = {
-  NODE_ENV,
-  JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret',
-};
--- frontend/src/context/CurrentUserContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-const CurrentUserContext = createContext(null);
-
-export const useCurrentUser = () => useContext(CurrentUserContext);
-
-export default CurrentUserContext;
