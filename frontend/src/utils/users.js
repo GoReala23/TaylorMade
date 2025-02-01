ba35ba288dec58acee0ab755758887d4e7ac89f3
Hassan Porter <90788434+GoReala23@users.noreply.github.com>
2025-02-01 15:53:57 -0500

    users

--- frontend/src/utils/users.js
+++ frontend/src/utils/users.js
@@ -2,19 +2,19 @@ const users = [
   {
     name: 'Admin',
     email: 'adminTest@example.com',
-    password: 'admin123',
+    password: 'Adminpw123',
     isAdmin: true,
   },
   {
     name: 'User',
     email: 'regUserTest@example.com',
-    password: 'user123',
+    password: 'password123',
     isAdmin: false,
   },
   {
     name: 'User 2',
     email: 'regUser2Test@example.com',
-    password: 'user123',
+    password: 'password123',
     isAdmin: false,
   },
 ];
